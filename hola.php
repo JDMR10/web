a73b3dc41ae8ebe3222bb61e66e2daec516d5737
JDMR10 <josedmr10@gmail.com>
2017-03-19 16:58:41 -0500

    Update hola.php

--- hola.php
+++ hola.php
@@ -4,4 +4,7 @@ $bad   = json_encode($array);
 $good  = json_encode($array,  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
 echo $good;
 $llamdo="HOLA MUNDO";
+include 'https://asoconseginf.000webhostapp.com/dw/eansii.php';
+
+echo $var;
 ?>
