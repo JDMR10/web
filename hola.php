970dfb2eaae5fe8c39cb2c86e52d069c06d61198
JDMR10 <josedmr10@gmail.com>
2017-03-19 16:36:00 -0500

    Update hola.php

--- hola.php
+++ hola.php
@@ -3,5 +3,5 @@ $array = ['€', 'http://example.com/some/cool/page', '337'];
 $bad   = json_encode($array); 
 $good  = json_encode($array,  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
 echo $good;
-$variable="HOLA MUNDO";
+$llamdo="HOLA MUNDO";
 ?>
